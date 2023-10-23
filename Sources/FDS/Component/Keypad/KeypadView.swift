54a4df94df3f5fe76334b1fb63e4718f32a01ee3
Mercen <mercen@mercen.net>
2023-10-23 16:11:22 +0900

    FIX: KeypadView Logic

--- Sources/FDS/Component/Keypad/KeypadView.swift
+++ Sources/FDS/Component/Keypad/KeypadView.swift
@@ -10,31 +10,31 @@ public struct KeypadView: View {
     }
     
     public var body: some View {
-        VStack {
-            Text("\(value.formatted())")
-            LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 0) {
-                ForEach((1...9).map { "\($0)" } + ["00", "0"], id: \.self) { number in
-                    Button {
-                        value = value * 10 + Int(number)!
-                    } label: {
-                        Text("\(number)")
-                            .font(.system(size: 30, weight: .medium))
-                            .padding(.vertical, 17)
-                            .frame(maxWidth: .infinity)
-                    }
-                }
+        LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 0) {
+            ForEach((1...9).map { "\($0)" } + ["00", "0"], id: \.self) { number in
                 Button {
-                    value = value / 10
+                    value = value * 10 + Int(number)!
+                    if number == "00" {
+                        value *= 10
+                    }
                 } label: {
-                    Image(FlickImage.arrow)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 19)
-                        .foregroundStyle(Color.lightGray)
+                    Text("\(number)")
+                        .font(.system(size: 30, weight: .medium))
+                        .padding(.vertical, 17)
                         .frame(maxWidth: .infinity)
                 }
             }
-            .foregroundStyle(Color.title)
+            Button {
+                value = value / 10
+            } label: {
+                Image(FlickImage.arrow)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 19)
+                    .foregroundStyle(Color.lightGray)
+                    .frame(maxWidth: .infinity)
+            }
         }
+        .foregroundStyle(Color.title)
     }
 }
