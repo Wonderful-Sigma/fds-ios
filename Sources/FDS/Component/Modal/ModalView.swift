0b38419bfca7f6d418681c11e986b5f314a1a20a
Mercen <mercen@mercen.net>
2023-10-23 14:39:56 +0900

    ADD: Modal Additional Transition

--- Sources/FDS/Component/Modal/ModalView.swift
+++ Sources/FDS/Component/Modal/ModalView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @available(macOS 12, iOS 15, *)
 public struct ModalView<C: View, V: View>: View {
-
+    
     @State var animatedPresentation: Bool
     @State var startOffset: CGFloat?
     @State var dragOffset: CGFloat = 0
@@ -21,59 +21,66 @@ public struct ModalView<C: View, V: View>: View {
     }
     
     public var body: some View {
-        ZStack(alignment: .bottom) {
-            view()
-                .onChange(of: isPresented) { newValue in
-                    withAnimation(.spring) {
-                        animatedPresentation = newValue
-                    }
+        view()
+            .onChange(of: isPresented) { newValue in
+                withAnimation(.spring(duration: 0.3)) {
+                    animatedPresentation = newValue
                 }
-            if animatedPresentation {
-                Color.black
-                    .opacity(0.5)
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-                    .onTapGesture {
-                        withAnimation(.spring) {
-                            isPresented = false
-                        }
-                    }
-                VStack(spacing: 0) {
-                    Capsule()
-                        .fill(Color.container)
-                        .frame(width: 135, height: 5)
-                    content()
-                        .padding(.vertical, 35)
-                }
-                .padding(12)
-                .frame(maxWidth: .infinity)
-                .background(Color.white)
-                .offset(y: dragOffset)
-                .transition(.move(edge: .bottom))
-                .simultaneousGesture(
-                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                        .onChanged { newValue in
-                            let offset = newValue.location.y
-                            if startOffset == nil {
-                                startOffset = offset
-                            }
-                            if let startOffset {
-                                dragOffset = -min(startOffset - offset, 0)
-                            }
-                        }
-                        .onEnded { _ in
-                            startOffset = nil
-                            if dragOffset < 100 {
-                                withAnimation(.spring) {
-                                    dragOffset = 0
-                                }
-                            } else {
+            }
+            .overlay(
+                GeometryReader { geometryProxy in
+                    ZStack(alignment: .bottom) {
+                        Color.black
+                            .opacity(animatedPresentation ? 0.5 : 0)
+                            .ignoresSafeArea()
+                            .onTapGesture {
                                 isPresented = false
                             }
+                        if animatedPresentation {
+                            Color.white
+                                .frame(height: -min(dragOffset, 0))
+                            let bottomSafeArea = geometryProxy.safeAreaInsets.bottom
+                            VStack(spacing: 0) {
+                                Capsule()
+                                    .fill(Color.container)
+                                    .frame(width: 135, height: 5)
+                                content()
+                                    .padding(.vertical, 35)
+                            }
+                            .padding(12)
+                            .padding(.bottom, bottomSafeArea)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.white)
+                            .offset(y: dragOffset)
+                            .transition(.move(edge: .bottom))
+                            .zIndex(1)
+                            .simultaneousGesture(
+                                DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                                    .onChanged { newValue in
+                                        let offset = newValue.location.y
+                                        if startOffset == nil {
+                                            startOffset = offset
+                                        }
+                                        if let startOffset {
+                                            let value = -(startOffset - offset)
+                                            dragOffset = value < 0 ? value / 5 : value
+                                        }
+                                    }
+                                    .onEnded { _ in
+                                        startOffset = nil
+                                        if dragOffset > 100 {
+                                            isPresented = false
+                                        }
+                                        withAnimation(.spring) {
+                                            dragOffset = 0
+                                        }
+                                    }
+                            )
                         }
-                )
-            }
-        }
+                    }
+                    .ignoresSafeArea()
+                }
+            )
     }
 }
 
@@ -83,10 +90,11 @@ struct ModalPreview: View {
     
     var body: some View {
         ZStack {
-            Color.red.ignoresSafeArea()
+            Color.red
         }
         .modal(isPresented: $isPresented) {
             Text("aa")
+                .frame(height: 100)
         }
     }
 }
