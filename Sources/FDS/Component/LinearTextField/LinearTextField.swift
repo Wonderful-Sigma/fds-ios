486a188da8f5557fc49b3ac13dbec235a324d05e
Mercen <mercen@mercen.net>
2023-11-15 16:33:22 +0900

    ADD: Animation for LinearTextField

--- Sources/FDS/Component/LinearTextField/LinearTextField.swift
+++ Sources/FDS/Component/LinearTextField/LinearTextField.swift
@@ -7,32 +7,39 @@ public struct LinearTextField: View {
   @Binding var text: String
   
   @FocusState private var focus: Bool
+  @State private var animatedFocus: Bool = false
   
   let placeholder: String
+  let isFirstResponder: Bool
   let isSecured: Bool
   let unit: String?
+  let keyboardType: UIKeyboardType
   
   public init(_ placeholder: String,
               text: Binding<String>,
+              isFirstResponder: Bool = false,
               isSecured: Bool = false,
-              unit: String? = nil) {
+              unit: String? = nil,
+              keyboardType: UIKeyboardType = .default) {
     self.placeholder = placeholder
     self._text = text
+    self.isFirstResponder = isFirstResponder
     self.isSecured = isSecured
     self.unit = unit
+    self.keyboardType = keyboardType
   }
   
   public var body: some View {
     VStack(alignment: .leading, spacing: 0) {
       Spacer()
-      if focus {
+      if animatedFocus {
         Text(placeholder)
           .scaleEffect(0.65, anchor: .leading)
-          .foregroundStyle(focus ? Color.blue500 : .lightGray)
+          .foregroundStyle(animatedFocus ? Color.blue500 : .lightGray)
           .matchedGeometryEffect(id: "text", in: animation)
       }
       ZStack(alignment: .leading) {
-        if !focus {
+        if !animatedFocus {
           Text(placeholder)
             .foregroundStyle(Color.lightGray)
             .matchedGeometryEffect(id: "text", in: animation)
@@ -52,29 +59,61 @@ public struct LinearTextField: View {
         }
       }
       Rectangle()
-        .fill(focus ? Color.blue500 : .lightGray)
+        .fill(animatedFocus ? Color.blue500 : .lightGray)
         .frame(height: 1.5)
         .padding(.top, 6)
     }
     .font(.system(size: 20))
     .frame(height: 70)
     .transition(.slide.combined(with: .opacity))
+    .onAppear {
+      if isFirstResponder {
+        focus = true
+      }
+    }
+    .onChange(of: focus) { newValue in
+      withAnimation(.easeInOut(duration: 0.2)) {
+        animatedFocus = newValue
+      }
+    }
   }
 }
 
 public extension LinearTextField {
   
+  @ViewBuilder func makeFirstResponder() -> LinearTextField {
+    LinearTextField(self.placeholder,
+                    text: self.$text,
+                    isFirstResponder: true,
+                    isSecured: self.isSecured,
+                    unit: self.unit,
+                    keyboardType: self.keyboardType)
+  }
+  
   @ViewBuilder func isSecured(_ condition: Bool) -> LinearTextField {
     LinearTextField(self.placeholder,
                     text: self.$text,
+                    isFirstResponder: self.isFirstResponder,
                     isSecured: condition,
-                    unit: self.unit)
+                    unit: self.unit,
+                    keyboardType: self.keyboardType)
   }
   
   @ViewBuilder func unit(_ string: String) -> LinearTextField {
     LinearTextField(self.placeholder,
                     text: self.$text,
+                    isFirstResponder: self.isFirstResponder,
                     isSecured: self.isSecured,
-                    unit: string)
+                    unit: string,
+                    keyboardType: self.keyboardType)
+  }
+  
+  @ViewBuilder func keyboardType(_ type: UIKeyboardType) -> LinearTextField {
+    LinearTextField(self.placeholder,
+                    text: self.$text,
+                    isFirstResponder: self.isFirstResponder,
+                    isSecured: self.isSecured,
+                    unit: self.unit,
+                    keyboardType: type)
   }
 }
